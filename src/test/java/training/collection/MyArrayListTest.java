fda080016e7dc3d2daa9206a73eb16f57d07fa02
Oleksij Onysymchuk <oleksij.onysymchuk@gmail.com>
2016-11-19 01:04:08 +0200

    Added test class with stubs

---
+++ src/test/java/training/collection/MyArrayListTest.java
@@ -0,0 +1,124 @@
+package training.collection;
+
+import org.junit.Test;
+
+/**
+ * Created by oleksij.onysymchuk@gmail on 19.11.2016.
+ */
+public class MyArrayListTest {
+    @Test
+    public void testSize() throws Exception {
+
+    }
+
+    @Test
+    public void testIsEmpty() throws Exception {
+
+    }
+
+    @Test
+    public void testContains() throws Exception {
+
+    }
+
+    @Test
+    public void testIterator() throws Exception {
+
+    }
+
+    @Test
+    public void testToArray() throws Exception {
+
+    }
+
+    @Test
+    public void testToArray1() throws Exception {
+
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+
+    }
+
+    @Test
+    public void testRemove() throws Exception {
+
+    }
+
+    @Test
+    public void testContainsAll() throws Exception {
+
+    }
+
+    @Test
+    public void testAddAll() throws Exception {
+
+    }
+
+    @Test
+    public void testAddAll1() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveAll() throws Exception {
+
+    }
+
+    @Test
+    public void testRetainAll() throws Exception {
+
+    }
+
+    @Test
+    public void testClear() throws Exception {
+
+    }
+
+    @Test
+    public void testGet() throws Exception {
+
+    }
+
+    @Test
+    public void testSet() throws Exception {
+
+    }
+
+    @Test
+    public void testAdd1() throws Exception {
+
+    }
+
+    @Test
+    public void testRemove1() throws Exception {
+
+    }
+
+    @Test
+    public void testIndexOf() throws Exception {
+
+    }
+
+    @Test
+    public void testLastIndexOf() throws Exception {
+
+    }
+
+    @Test
+    public void testListIterator() throws Exception {
+
+    }
+
+    @Test
+    public void testListIterator1() throws Exception {
+
+    }
+
+    @Test
+    public void testSubList() throws Exception {
+
+    }
+
+}
